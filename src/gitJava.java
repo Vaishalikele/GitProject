de962f0fe217f9b85df11fb247f03da531248914
vaishu <vaishukele51@gmail.com>
2021-11-23 15:22:19 +0530

    created the project. now you can add one more file in the project

---
+++ src/gitJava.java
@@ -0,0 +1,10 @@
+
+public class gitJava {
+
+	public static void main(String[] args) {
+		System.out.println("hi vaishali");
+		System.out.println(" project is done");
+		System.out.println(" done");
+	}
+
+}
